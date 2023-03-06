ae84fcffe730e2ca02487c897398c02f9c2c1a3b
Yokiebob <126304146+Yokiebob@users.noreply.github.com>
2023-03-06 16:54:25 -0600

    Update cheapWebgl.js

--- cheapWebgl.js
+++ cheapWebgl.js
@@ -16,5 +16,35 @@ void main(){
 }`;
 function createProgram(fsh){
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vsh);
+  gl.shaderSource(vertexShader,vsh);
+  gl.compileShader(vertexShader);
+  
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader,fsh);
+  gl.compileShader(fragmentShader);
+  
+  var program = gl.createProgram();
+  gl.attachShader(program,vertexShader);
+  gl.attachShader(program,fragmentShader);
+  gl.linkProgram(program);
+  return program;
+}
+function setUpProgram(program){
+  var positionAttributeLocation = gl.getAttribLocation(program,'pos');
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer);
+  var screen = [
+    1,1,
+    -1,1,
+    1,-1,
+    
+    -1,1,
+    1,-1,
+    -1,-1
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(screen),gl.STATIC_DRAW);
+  var vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation,2,gl.FLOAT,false,0,0);
 }
