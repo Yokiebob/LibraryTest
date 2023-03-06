d70f6af1981deee8783a7291918981a5a0ef93df
Yokiebob <126304146+Yokiebob@users.noreply.github.com>
2023-03-06 15:54:08 -0600

    Update cheapWebgl.js

--- cheapWebgl.js
+++ cheapWebgl.js
@@ -1,6 +1,12 @@
 var webglCanvas = new OffscreenCanvas(400,400);
 var gl = webglCanvas.getContext('webgl2');
-var vertexShader = 
+if(!gl){
+  alert('You need webgl2 for this to work!');
+  println('you need webgl2 for this to work!');
+  console.log('You need webgl2 for this to work!');
+  debug('you need webgl2 for this to work!');
+}
+var vsh = 
 `#version 300 es
 
 in vec2 pos;
@@ -8,3 +14,7 @@ in vec2 pos;
 void main(){
   gl_Position = pos;
 }`;
+function createProgram(fsh){
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vsh);
+}
